zOrg1331/kernel-image-cuboxi

ARM: Convert platform reservations to use LMB rather than bootmem
Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

arch/arm/kernel/setup.c
@@ -714,7 +714,7 @@ void __init setup_arch(char **cmdline_p)
 
 	parse_early_param();
 
-	arm_lmb_init(&meminfo);
+	arm_lmb_init(&meminfo, mdesc);
 
 	paging_init(mdesc);
 	request_standard_resources(&meminfo, mdesc);

arch/arm/mach-integrator/core.c
@@ -14,7 +14,7 @@
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <linux/bootmem.h>
+#include <linux/lmb.h>
 #include <linux/sched.h>
 #include <linux/smp.h>
 #include <linux/termios.h>
@@ -225,5 +225,5 @@ EXPORT_SYMBOL(cm_control);
  */
 void __init integrator_reserve(void)
 {
-	reserve_bootmem(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET, BOOTMEM_DEFAULT);
+	lmb_reserve(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET);
 }

dhiru1602/samsung-kernel-latona

arch/arm/kernel/setup.c
@@ -716,7 +716,7 @@ void __init setup_arch(char **cmdline_p)
 
 	parse_early_param();
 
-	arm_memblock_init(&meminfo);
+	arm_memblock_init(&meminfo, mdesc);
 
 	paging_init(mdesc);
 	request_standard_resources(&meminfo, mdesc);

arch/arm/mach-integrator/core.c
@@ -14,7 +14,7 @@
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <linux/bootmem.h>
+#include <linux/memblock.h>
 #include <linux/sched.h>
 #include <linux/smp.h>
 #include <linux/termios.h>
@@ -225,5 +225,5 @@ EXPORT_SYMBOL(cm_control);
  */
 void __init integrator_reserve(void)
 {
-	reserve_bootmem(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET, BOOTMEM_DEFAULT);
+	memblock_reserve(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET);
 }